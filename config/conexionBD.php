86e6b0bb0f05e4f16f7465c9ad2b7399c1e2fb8e
william9969 <wsinchi69@gmail.com>
2020-05-23 22:03:56 -0500

    Pagina web

---
+++ config/conexionBD.php
@@ -0,0 +1,16 @@
+<?php
+    $db_servername = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "hipermedial";
+
+    $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+    $conn->set_charset("utf8");
+
+    # Probar conexión
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }else{
+            
+        }
+?>
