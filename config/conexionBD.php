ff125d2ac5d73b595bef5ed75619c1c6b1972cc8
¨jessica-maribel¨ <jessicaguncay45@gmail.com>
2020-05-24 14:37:35 -0500

    editar paginas html

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -2,7 +2,7 @@
     $db_servername = "localhost";
     $db_username = "root";
     $db_password = "";
-    $db_name = "hipermedial";
+    $db_name = "agenda";
 
     $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
     $conn->set_charset("utf8");
