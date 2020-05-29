a70048dd996789dbdadf39c171cf62fcabca0fc3
jessica-maribel <jessicaguncay45@gmail.com>
2020-05-28 23:49:00 -0500

    creacion tabla telefono con usuario

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -14,21 +14,25 @@
     $direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8') : null;
     $tip_telefono = isset($_POST["tip_telefono"]) ? mb_strtoupper(trim($_POST["tip_telefono"]), 'UTF-8') : null;
     $telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]): null;
+    $operadora = isset($_POST["operadora"]) ? mb_strtoupper(trim($_POST["operadora"]), 'UTF-8') : null;
     $correo = isset($_POST["correo"]) ? trim($_POST["correo"]): null;
     $fechaNacimiento = isset($_POST["fechaNacimiento"]) ? trim($_POST["fechaNacimiento"]): null;
     $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
     $sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion','$fechaNacimiento','$correo', MD5('$contrasena'))";
+    $res = $conn->query($sql);
+    $id_user=mysqli_insert_id($conn);
 
-    if ($conn->query($sql) === TRUE) {
-        echo "<p>Se ha creado los datos personales correctamemte!!!</p>";
-    } else {
+    if ($res === TRUE) {
+        $sqlQ = "INSERT INTO telefono VALUES (0, '$tip_telefono', '$telefono',$id_user , '$operadora')";
+        $resS = $conn->query($sqlQ);
+   } else {
         if($conn->errno == 1062){
             echo "<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema </p>";
         }else{
             echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
         }
     }
-    
+
      //cerrar la base de datos
      $conn->close();
      echo "<a href='../../vista/Docs/Validaciones/validacion.html'>Regresar</a>";
