1e3544944fb9008ac3717f212eaf6504c1871f83
william9969 <wsinchi69@gmail.com>
2020-05-31 22:40:41 -0500

    validacion

--- admin/vista/JavaScript/validacion.js
+++ admin/vista/JavaScript/validacion.js
@@ -439,7 +439,6 @@ function busquedaTelefono(){
         }
         xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-        alert("llegue");
     
         document.getElementById("informacion").innerHTML = this.responseText;
         }
