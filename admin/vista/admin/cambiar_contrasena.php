c4d82b5d9e8a39993badcd7a750f1d4a1abd02bf
jessica-maribel <jessicaguncay45@gmail.com>
2020-06-01 08:59:24 -0500

    validacion de contraseña con js

--- admin/vista/admin/cambiar_contrasena.php
+++ admin/vista/admin/cambiar_contrasena.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Modificar contrasena de persona</title>
+    <script type="text/javascript" src="../JavaScript/validacion.js"></script>
     <link href="../../../public/vista/CSS/CSSADMINISTRADOR/cambiar-contrasena.css" type="text/css"  rel="stylesheet"/>
 </head>
 <body>
@@ -13,12 +14,14 @@
 
     <input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>" />
     <label for="cedula">Contraseña Actual (*)</label>
-    <input type="password" id="contrasena1" name="contrasena1" value="" required
+    <input type="password" id="contrasena1" onkeyup="return validarContraseña(this)" name="contrasena1" value=""  required
     placeholder="Ingrese su contraseña actual ..."/>
+    <span id="mensajeContrasena" class="error"></span>
     <br>
     <label for="cedula">Contraseña Nueva (*)</label>
-    <input type="password" id="contrasena2" name="contrasena2" value="" required
+    <input type="password" id="contrasena2" onkeyup="return validarContraseña(this)" name="contrasena2" value=""   required
     placeholder="Ingrese su contraseña nueva ..."/>
+    <span id="mensajeContrasena" class="error"></span>
     <br>
     <input type="submit" id="modificar" name="modificar" value="Modificar" />         
     <input type="reset" id="cancelar"  onclick=history.back(); name="cancelar" value="Cancelar" />     
